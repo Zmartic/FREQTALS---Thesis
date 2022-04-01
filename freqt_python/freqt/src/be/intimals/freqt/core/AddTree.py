11c52c57062a90968b6c7fe35f64af8e4a6b77b4
Zmartic <arnaudspits@gmail.com>
2022-04-01 12:26:58 +0200

    add good comment to AddTree.py

--- freqt_python/freqt/src/be/intimals/freqt/core/AddTree.py
+++ freqt_python/freqt/src/be/intimals/freqt/core/AddTree.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
-
 from freqt.src.be.intimals.freqt.core.CheckSubtree import check_subtree
+
+"""
+ Set of function used to add pattern to some structure
+ Usually used inside add_tree() function from FreqT's implementation
+"""
 
 
 def add_maximal_pattern(pat, proj, mfp, not_maximal_set):
     """
-     * add maximal patterns
-     * @param: pat, FTArray
-     * @param: projected, Projected
-     * @param: _MFP_dict, a dictionary with FTArray as keys and String as values
+     Add a pattern to the set of maximal frequent pattern (only keep maximal pattern = super-tree)
+        * if pat is a sub-tree of an other pattern -> don't add pat
+        * if an other pattern is a sub-tree of pat -> remove this other pattern
+    :param pat:  FTArray,   the pattern requested to be added to mfp
+    :param proj: Projected, the projection of pat
+    :param mfp:  Dict(FTArray, Projected), the set of maximal frequent pattern
+    :param not_maximal_set: Dict(FTArray), set of pattern previously encounter that were not maximal
     """
     if len(mfp) != 0:
         if pat in not_maximal_set:
@@ -32,10 +39,15 @@ def add_maximal_pattern(pat, proj, mfp, not_maximal_set):
 
 def add_root_ids(pat, proj, root_ids_list):
     """
-     * store root occurrences of pattern
-     * @param: pat, FTArray
-     * @param: projected, Projected
-     * @param: _rootIDs_dict, a dictionary with Projected as keys and FTArray as values
+     Add a pattern to the set of root occurrences (only keep sub-set of occurrences)
+        * if the set of occurrence of pat is a super-set of an other pattern
+          -> don't add the occurrences of pat
+        * if the set of occurrence of an other pattern is a super-set of pat
+          -> remove the occurrences of this other pattern
+    :param pat:  FTArray,   the pattern requested to be added to root_ids_list
+    :param proj: Projected, the projection of pat
+    :param root_ids_list: List(Int, Set()), the list of roots with their set of occurrences
+                          Occurrence = Tuple(location_id, root_location)
     """
     # set of root occurrences of current pattern
     root_occ1 = {(loc.get_location_id(), loc.get_root()) for loc in proj.get_locations()}
@@ -56,31 +68,38 @@ def add_root_ids(pat, proj, root_ids_list):
 
 def add_high_score_pattern(pat, proj, score, hsp, num_pattern):
     """
-     * add pattern to the list of N-highest chi-square score patterns
-     * @param: pat, FTArray
-     * @param: projected, Projected
-     * @param: _HSP_dict, dictionary with FTArray as keys and Projected as values
+     Add a pattern to the set of N-highest chi-square score patterns
+    :param pat:  FTArray,   the pattern requested to be added to hsp
+    :param proj: Projected, the projection of pat
+    :param score: Float, the pattern chi-square score
+    :param hsp: Dict(FTArray, Tuple), the set of high chi-square score patterns
+                Tuple(Projected, Float) for each pattern we store its projection and its score
+    :param num_pattern: int, define the maximum number of pattern that can be store in hsp
     """
+
     if pat not in hsp:
         if len(hsp) >= num_pattern:
-            min_pat = get_min_score_pattern(hsp, score)
-            if min_pat is not None:
-                # remove minScore pattern
-                del min_pat
-                # add new pattern
-                hsp[pat] = (proj, score)
+            min_pat = get_min_score_pattern(hsp, pat, score)
+            # remove minScore pattern
+            del min_pat
+            # add new pattern
+            hsp[pat] = (proj, score)
         else:
             # add new pattern
             hsp[pat] = (proj, score)
 
 
-def get_min_score_pattern(hsp, current_score):
+def get_min_score_pattern(hsp, current_pat, current_score):
     """
-     * get a pattern which has minimum chi-square score in the list of patterns
-     * @param: _HSP_dict: dictionary with FTArray as key and Projected as values
+     Get the pattern which has minimum chi-square score in the list of patterns (hsp + current_pattern)
+    :param hsp: Dict(FTArray, Tuple), the set of high chi-square score patterns
+                Tuple(Projected, Float) for each pattern we store its projection and its score
+    :param current_pat: FTArray, pattern that is currently added to hsp
+    :param current_score: score of the pattern that is currently added to hsp
+    :return: the minimum score pattern
     """
     min_score = current_score
-    min_score_pattern = None
+    min_score_pattern = current_pat
 
     for key in hsp:
         score = hsp[key][1]
